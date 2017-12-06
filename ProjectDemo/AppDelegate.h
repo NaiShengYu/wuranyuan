816165052be73e05aa17645881f7228edf1e7e55
俞乃胜, Stephen <stephenyu@developmentmac0.local>
2017-12-06 13:06:34 +0800

    Initial Commit

---
+++ ProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectDemo
+//
+//  Created by 俞乃胜, Stephen on 2017/12/6.
+//  Copyright © 2017年 俞乃胜, Stephen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
